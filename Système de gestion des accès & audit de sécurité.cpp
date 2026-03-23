e04f780449a9b333355b0f4c2ed96c583e7503cb
Mikailou Cedric Toure <mikailoucedrictoure@gmail.com>
2026-03-23 13:48:21 -0300

    initialiation du projet

---
+++ Système de gestion des accès & audit de sécurité.cpp
@@ -0,0 +1,18 @@
+//Toure Mikailou Cedric  
+//Système de gestion des accès & audit de sécurité
+//Département d'informatique
+//Université de Moncton
+
+
+
+// Ce programme implémente un système de gestion des accès et d'audit de sécurité.
+
+
+#include <iostream>
+#include <string>
+#include<stdexcept>
+#include<fstream>
+#include<ctime>
+using namespace std;
+
+
